919615d90dc00cea931095e4f83cd347e051595a
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-07 20:10:04 +0300

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -37,12 +37,9 @@ app.get('/todos/:id', (req, res) => {
         if (!todo)
             return res.status(404).send();
         res.send({ todo });
-    }, err => {
-        res.status(404).send("");
     }).catch(e => {
         res.status(400).send();
     });
-
 })
 
 

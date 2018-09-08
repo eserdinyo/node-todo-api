ee191f09fe6bdd197e8be81fe571df81c4ad7a6d
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-08 22:53:15 +0300

    Setup dest and development envs

--- server/config/config.js
+++ server/config/config.js
@@ -8,4 +8,6 @@ if (env == 'development') {
 else if (env == 'test') {
     process.env.PORT = 3000;
     process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest'
+} else {
+    process.env.MONGODB_URI = 'mongodb://todoapp:123qweasd@ds149682.mlab.com:49682/todo-app';
 }

010ebf70f42f31962bf54ca19d7c9811f2ee0118
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-08 15:07:12 +0300

    a change in mongoose.js

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://<todoapp>:<1231233fuck>@ds149682.mlab.com:49682/todo-app", { useNewUrlParser: true });
+mongoose.connect("mongodb://todoapp:123qweasd@ds149682.mlab.com:49682/todo-app", { useNewUrlParser: true });
 
 module.exports = { mongoose }

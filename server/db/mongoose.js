984ad042f4a67885a62a898995566293e1a0dad4
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-08 22:50:54 +0300

    Setup dest and development envs

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,6 +2,6 @@ const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 //mongodb://todoapp:123qweasd@ds149682.mlab.com:49682/todo-app
-mongoose.connect("mongodb://localhost:27017/TodoApp", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
 
 module.exports = { mongoose }

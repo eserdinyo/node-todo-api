f91cb714ae0a2aaa9a7dcd6ae3622769af540c62
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-10 10:41:05 +0300

    Add user auth

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
+const { User } = require('./models/user');
 
 const app = express();
 const port = process.env.PORT;
@@ -88,6 +89,19 @@ app.patch('/todos/:id', (req, res) => {
     })
 
 
+})
+
+app.post('/users', (req, res) => {
+
+    const body = _.pick(req.body, ['email', 'password']);
+
+    const user = new User(body);
+
+    user.save().then(user => {
+        res.send(user);
+    }).catch(e => {
+        res.status(400).send(e);
+    })
 })
 
 app.listen(port, () => {
